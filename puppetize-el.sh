f826e237e2428f0a8c9e1f7646c28e6886b824c1
limed <edward.lim@oregonstate.edu>
2012-06-14 14:50:45 -0700

    Added some extra stuff for puppetize-el script, now disables selinux at the end

--- puppetize-el.sh
+++ puppetize-el.sh
@@ -6,13 +6,18 @@
 
 export http_proxy='http://proxy.oregonstate.edu:3128'
 
+echo "proxy=http://proxy.oregonstate.edu:3128" >> /etc/yum.conf
+
 yum clean all
-yum install -y ntp ntpdate redhat-lsb openssh-clients
+yum install -y ntp ntpdate redhat-lsb openssh-clients facter
+yum remove -y bluez-* ppp NetworkManager sysklogd chkfontpath sendmail b43-* xorg-x11-*
 
 PUPPET_REPO="http://yum.puppetlabs.com"
 ARCH=`uname -i`
 RELEASE=`lsb_release -r | awk '{print $2}'`
 PUPPET_SERVER=$1
+PROXY_SERVER="proxy.oregonstate.edu"
+PROXY_PORT='3128'
 
 if [ -z ${PUPPET_SERVER} ]; then
     echo "Usage: $0 [puppetmaster]"
@@ -20,9 +25,9 @@ if [ -z ${PUPPET_SERVER} ]; then
 fi
 
 if [ $(echo "${RELEASE} >= 6" | bc ) -eq 1 ]; then
-    rpm -ivh "http://yum.puppetlabs.com/el/6/products/${ARCH}/puppetlabs-release-6-1.noarch.rpm"
+    rpm -ivh --httpproxy ${PROXY_SERVER} --httpport ${PROXY_PORT} "${PUPPET_REPO}/el/6/products/${ARCH}/puppetlabs-release-6-1.noarch.rpm"
 else
-    rpm -ivh "http://yum.puppetlabs.com/el/5/products/${ARCH}/puppetlabs-release-5-1.noarch.rpm"
+    rpm -ivh --httpproxy ${PROXY_SERVER} --httpport ${PROXY_PORT} "${PUPPET_REPO}/el/5/products/${ARCH}/puppetlabs-release-5-1.noarch.rpm"
 fi
 
 # Adjust time
@@ -49,3 +54,6 @@ cat > /etc/puppet/puppet.conf << PUPPETCONF
     pluginsync  = true
 PUPPETCONF
 
+# Disable selinux
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+setenforce 0
